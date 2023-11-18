4fb15b9c1ee81e92f76f51e40ae44f57af4e3650
Franklin da Silva Batista <frsbatist@gmail.com>
2023-11-17 21:36:35 -0300

    21:36    17/11/2023
    Calcular e apresentar a força da senha com javascript

--- adm/app/adms/assets/Js/custom_adms.js
+++ adm/app/adms/assets/Js/custom_adms.js
@@ -1,10 +1,11 @@
+// window.alert ("carregou");
 const formNewUser = document.getElementById("form-new-user");
 if(formNewUser){
     formNewUser.addEventListener("submit", async(e) =>{
 
         // document.getElementById("msg").innerHTML = "<p style='color: #0f0;'>Usuário cadastrado com sucesso!</p>";
 
-        //Recebe o valor do campo 
+        //Recebe o valor do campo NAME
         var name = document.querySelector('#name').value;
         if(name === ""){
             e.preventDefault();
@@ -13,7 +14,7 @@ if(formNewUser){
             
             return;
         }
-        //Recebe o valor do campo 
+        //Recebe o valor do campo E-MAIL
         var email = document.querySelector('#email').value;
         if(email === ""){
             e.preventDefault();
@@ -22,12 +23,38 @@ if(formNewUser){
             
             return;
         }
-        //Recebe o valor do campo 
+        //Recebe o valor do campo SENHA
         var password = document.querySelector('#password').value;
         if(password === ""){
             e.preventDefault();
 
             document.getElementById("msg").innerHTML = "<p style='color: #f00;'>Necessário preencher campo Senha!</p>";
+            
+            return;
+        }
+        //Verifica se o campo SENHA possui 6 caracteres
+        if(password.length < 6){
+            e.preventDefault();
+
+            document.getElementById("msg").innerHTML = "<p style='color: #f00;'>Erro: A Senha deve conter (NO MÍNIMO) 6 caracteres!</p>";
+            
+            return;
+        }
+
+        //Verifica se o campo SENHA não possui núeros repetidos
+        if(password.match(/([1-9]+)\1{1,}/)) {
+            e.preventDefault();
+
+            document.getElementById("msg").innerHTML = "<p style='color: #f00;'>Erro: A Senha não deve ter números repetidos!</p>";
+            
+            return;
+        }
+
+        //Verifica se o campo SENHA possui letras
+        if(!password.match(/[A-Za-z]/)) {
+            e.preventDefault();
+
+            document.getElementById("msg").innerHTML = "<p style='color: #f00;'>Erro: A Senha deve conter pelo menos uma letra!</p>";
             
             return;
         }
@@ -39,10 +66,8 @@ if(formNewUser){
 const formLogin = document.getElementById("form-login"); 
 if(formLogin){
     formLogin.addEventListener("submit", async(e) => {
-        // e.preventDefault();
 
-        // document.getElementById("msg").innerHTML = "<p style='color: #0f0;'>Acessou: Validar form login</p>";
-        //Recebe o valor do campo 
+        //Recebe o valor do campo USER
         var user = document.querySelector('#user').value;
         if(user === ""){
             e.preventDefault();
@@ -51,7 +76,7 @@ if(formLogin){
             
             return;
         }
-        //Recebe o valor do campo 
+        //Recebe o valor do campo SENHA
         var password = document.querySelector('#password').value;
         if(password === ""){
             e.preventDefault();
@@ -60,6 +85,7 @@ if(formLogin){
             
             return;
         }
+        
     })
 
 }
